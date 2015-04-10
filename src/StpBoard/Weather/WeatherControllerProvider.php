3fcf2b5f049f02fb83a2212e9738abbb5800413c
Michal Plachta <michal.plachta@schibsted.pl>
2015-04-10 12:02:52 +0200

    Initial commit

---
+++ src/StpBoard/Weather/WeatherControllerProvider.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace StpBoard\Weather;
+
+use Cmfcmf\OpenWeatherMap;
+use Guzzle\Http\Client;
+use Silex\Application;
+use Silex\ControllerCollection;
+use Silex\ControllerProviderInterface;
+use StpBoard\Base\BoardProviderInterface;
+use StpBoard\Base\TwigTrait;
+
+class WeatherControllerProvider implements ControllerProviderInterface, BoardProviderInterface
+{
+    use TwigTrait;
+
+    /**
+     * Returns route prefix, starting with "/"
+     *
+     * @return string
+     */
+    public static function getRoutePrefix()
+    {
+        return '/weather';
+    }
+
+    /**
+     * @param Application $app An Application instance
+     *
+     * @return ControllerCollection A ControllerCollection instance
+     */
+    public function connect(Application $app)
+    {
+        $this->initTwig(__DIR__ . '/views');
+        $controllers = $app['controllers_factory'];
+
+        $controllers->get(
+            '/',
+            function (Application $app) {
+                $request = $app['request'];
+                $data = [];
+
+                $city = $request->get('city');
+                if (empty($city)) {
+                    return $this->twig->render('error.html.twig');
+                }
+
+                $openWeatherMap = new OpenWeatherMap();
+                try {
+                    $weather = $openWeatherMap->getWeather($city, 'metric');
+                } catch (\Exception $e) {
+                    return $this->twig->render('error.html.twig');
+                }
+
+                return $this->twig->render(
+                    'weather.html.twig',
+                    [
+                        'id' => $request->get('id'),
+                        'temperature' => (int)$weather->temperature->now->getValue(),
+                        'icon' => $weather->weather->icon
+                    ]
+                );
+            }
+        );
+
+        return $controllers;
+    }
+}
